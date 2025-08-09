8e0347777e2f4aa6838b8dcb02bf4b98117ed172
Weretik <vitaliythupin@gmail.com>
2025-08-09 17:53:13 +0300

    feat(presentation-client): scaffold Presentation.Client layer structure
    - Added Components folder with ExampleAggregate (partials, sections, widgets) and Shared widgets
    - Created Pages folder for Razor pages
    - Added _Imports.razor, GlobalUsing.cs, and Program.cs for initial Blazor setup

---
+++ src/Presentation/Presentation.Client/GlobalUsing.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+global using Presentation.Shared.Common;
+global using Presentation.Shared.States;
+global using MudBlazor.Services;
--- src/Presentation/Presentation.Client/Program.cs
+++ src/Presentation/Presentation.Client/Program.cs
@@ -1,5 +1,10 @@
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped<StateContainer>();
+builder.Services.AddScoped<BurgerMenuState>();
+builder.Services.AddScoped<IState>(sp => sp.GetRequiredService<BurgerMenuState>());
+
+
+builder.Services.AddMudServices();
 
 await builder.Build().RunAsync();
